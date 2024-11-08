5c40ec858b3db5829ebd7db7fea1f5057eb80f9e
Sripranathi26 <141115673+Sripranathi26@users.noreply.github.com>
2024-11-08 11:54:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -52,5 +52,5 @@ app.post('/submit_login', (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`http://cluster.rugql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`);
 });
